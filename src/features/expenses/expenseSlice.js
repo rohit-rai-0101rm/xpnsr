8c85814dbf0da0028ba84bcfc1f1f5f9c14f19eb
rohit rai <rohitrai.dev@gmail.com>
2021-12-18 11:38:37 +0530

    store and reducers defined

---
+++ src/features/expenses/expenseSlice.js
@@ -0,0 +1,11 @@
+import {createSlice} from "@reduxjs/toolkit"
+export const expenseSlice=createSlice({
+    name:"expense",
+    initialState:{
+
+    },
+    reducers:{
+
+    }
+})
+export default expenseSlice.reducer
